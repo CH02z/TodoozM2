161aead8a17408ea15188cd433f920f1b0e2108f
Chris Zimmermann <chris@Chriss-Mac-mini.home.intra>
2020-12-30 20:27:40 +0100

    ngx translate implementet, login translations, still more to come

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/app/Services/auth.service';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,12 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class AppComponent {
   title = 'todooz2';
 
-  constructor(public authService: AuthService, private router: Router, public af: AngularFireAuth) {
+  constructor(public authService: AuthService, private router: Router, public af: AngularFireAuth, public translate: TranslateService) {
+     // this language will be used as a fallback when a translation isn't found in the current language
+     translate.setDefaultLang('en');
+     // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('en');
+    
     this.af.authState.subscribe(user => {
       if (user) {
         //do smt
