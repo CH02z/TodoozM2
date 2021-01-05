92493fb961452aebb53e83947b772a073b62f65d
Chris Zimmermann <chris@Chriss-Mac-mini.home.intra>
2021-01-05 20:39:06 +0100

    firebase config + bug fix translate

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyBDt_LrMLe12YY4mG48VNZLKk8nzGJk5rE',
+    authDomain: 'todoozm2.firebaseapp.com',
+    databaseURL: 'https://todoozm2.firebaseio.com',
+    projectId: 'todoozm2',
+    storageBucket: 'todoozm2.appspot.com',
+    messagingSenderId: '133319236110',
+    appId: '1:133319236110:web:c1dab8bfdb233665b33a5f',
+    measurementId: 'G-7QQCL86C5F'
+  }
 };
