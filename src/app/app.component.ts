c88a49f7292b2e82ff4d9b84a73742417acfd653
Chris Zimmermann <chris@Chriss-Mac-mini.home.intra>
2021-01-05 12:17:34 +0100

    added google sign in, auth guard fix, other login improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/Services/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { LanguageService } from './Services/language.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,20 +16,36 @@ export class AppComponent {
 
   title = 'todooz2';
   uid = '';
+  private subscriptions: Subscription[] = [];
 
   constructor(public authService: AuthService,
               public af: AngularFireAuth,
-              public langService: LanguageService) {   
-    
-    this.af.authState.subscribe(user => {
-      if (user) {
-        this.uid = user.uid;
-        this.langService.setDefaulLang();
-      }
-    });
+              public langService: LanguageService,
+              private router: Router) {  
+    this.subscriptions.push(
+      this.af.authState.subscribe(user => {
+        if (user) {
+          this.uid = user.uid;
+          this.langService.setDefaulLang();
+        }
+      })
+    );
   }
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    setTimeout(() => {
+      this.subscriptions.forEach(subscription => subscription.unsubscribe());
+    }, 500);
+    
+    this.authService.logout();
+    setTimeout(() => {
+      if (this.router.url != "/resetpw") {
+        this.router.navigateByUrl('/login');
+      }
+    }, 1000);
   }
 
 
