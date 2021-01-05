7973429067876c8b6092831a5e41ab73f7f41841
Chris Zimmermann <chris@Chriss-Mac-mini.home.intra>
2021-01-05 12:23:44 +0100

    removed imports

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 
